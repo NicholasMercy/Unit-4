b0650674dd2e841ba34339bfbb34489ecbe2ab61
Nicholas <NicholasNaicker@hotmail.com>
2023-05-06 17:28:07 +0200

    Finished Unit 4.4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,6 +18,10 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(this.transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
         Vector3 lookDirection = (playerObj.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection*speed);
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,18 +7,37 @@ public class SpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     void Start()
     {
-        
 
-        Instantiate(enemyPrefab, GenerateRandomSpawnPos(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateRandomSpawnPos(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateRandomSpawnPos(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int spawnEnemies)
+    {
+        for (int i = 0;i <spawnEnemies;i++)
+        {
+            Instantiate(enemyPrefab, GenerateRandomSpawnPos(), enemyPrefab.transform.rotation);
+        }
         
     }
 
